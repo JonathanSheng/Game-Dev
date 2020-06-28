44bf42f58bba6cc2516bb6c17e6f08c88735261e
Jonathan Sheng <js165@rice.edu>
2020-06-27 20:53:57 -0400

    basic menu, need og images and design

---
+++ TitleScreen.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/StartGame.grab_focus()
+
+func _physics_process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/StartGame.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/StartGame.grab_focus()
+		
+
+
+func _on_StartGame_pressed():
+	get_tree().change_scene("Nexus.tscn")
+
+func _on_QuitGame_pressed():
+	get_tree().quit()
